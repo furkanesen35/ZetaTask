ddee36fffc419aa613413aa9f97ac049aeadf2ae
furkan <furkanesen35@gmail.com>
2022-10-04 15:43:24 +0200

    api fetched

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from "react";
+import axios from "axios"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ const [fetched, setFetched] = useState([]);
+ useEffect(() => {
+  getData();
+ }, [])
+ const getData = () => {
+  axios.get("https://633acdc6471b8c3955755ac0.mockapi.io/blog").then(res=>setFetched(res.data))
+ }
+ console.log(fetched);
+ 
+ return (
+  <div className="App">
+   <form action="">
+    <table>
+     <thead>
+      <tr>
+        <th>id</th>
+        <th>name</th>
+        <th>description</th>
+        <th>action</th>
+      </tr>
+     </thead>
+    </table>
+   </form>
+  </div>
+ );
 }
 
 export default App;
